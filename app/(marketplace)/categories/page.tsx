383b3547f2313839d4ea80a54de459ef1d3e22e7
Devsanjay12 <sanjaykumar2k04@gmail.com>
2025-12-12 00:21:54 +0530

    Fix categories prerender error

--- app/(marketplace)/categories/page.tsx
+++ app/(marketplace)/categories/page.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@/components/shared/header";
 import { CategoryList } from "@/components/marketplace/category-list";
 
+export const dynamic = 'force-dynamic';
+
 export default function CategoriesPage() {
     return (
         <div className="min-h-screen bg-background">
